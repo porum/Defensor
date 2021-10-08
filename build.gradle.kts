50e8d5d29088a1e82e19baededd4ecc06f1108b9
porum <sunguobao12@gmail.com>
2021-10-08 09:27:31 +0800

    release v1.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,18 +3,10 @@ ext {
   this["minVersion"] = 21
   this["targetVersion"] = 29
   this["buildTools"] = "29.0.1"
-
-  this["tests"] = HashMap<String, Any>().apply {
-    this["support-runner"] = "com.android.support.test:runner:1.0.2"
-  }
-
-  this["appcompat"] = "com.android.support:appcompat-v7:28.0.0"
-  this["androidx"] = "androidx.appcompat:appcompat:1.0.2"
-  this["gson"] = "com.google.code.gson:gson:2.8.6"
 }
 
-val commonConfigure = project.file("common_configure.gradle.kts")
-val publishConfigure = project.file("publish_configure.gradle.kts")
+val commonConfigure = project.file("common.gradle.kts")
+val publishConfigure = project.file("publishing.gradle.kts")
 
 subprojects {
   repositories {
