0a333161bf2c89fb21e7ed0b6be112b22e15a50e
porum <sunguobao12@gmail.com>
2022-04-25 12:28:14 +0800

    if parse primitive type has been wrapped by trycatch block, just output error message, don't return the default value.

--- defensor-gradle-plugin/src/main/java/com/panda912/defensor/plugin/internal/interceptor/PrimitiveInterceptor.kt
+++ defensor-gradle-plugin/src/main/java/com/panda912/defensor/plugin/internal/interceptor/PrimitiveInterceptor.kt
@@ -3,10 +3,14 @@ package com.panda912.defensor.plugin.internal.interceptor
 import com.panda912.defensor.plugin.internal.BytecodeInterceptor
 import com.panda912.defensor.plugin.internal.Output
 import com.panda912.defensor.plugin.internal.visitor.BaseClassReader
-import com.panda912.defensor.plugin.internal.visitor.BaseClassVisitor
-import com.panda912.defensor.plugin.internal.visitor.BaseMethodVisitor
 import com.panda912.defensor.plugin.utils.*
-import org.objectweb.asm.*
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.ClassWriter
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.Type
+import org.objectweb.asm.tree.AbstractInsnNode.METHOD_INSN
+import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.MethodInsnNode
 
 /**
  * Created by panda on 2021/9/15 11:20
@@ -17,55 +21,51 @@ class PrimitiveInterceptor : BytecodeInterceptor {
     val input = chain.request()
 
     val cr = BaseClassReader(input.bytes)
-    val cw = ClassWriter(cr, ClassWriter.COMPUTE_MAXS)
-    cr.accept(object : BaseClassVisitor(cw) {
-      override fun visitMethod(
-        access: Int,
-        name: String?,
-        descriptor: String?,
-        signature: String?,
-        exceptions: Array<out String>?
-      ): MethodVisitor {
-        val mv = super.visitMethod(access, name, descriptor, signature, exceptions)
-        return PrimitiveMethodVisitor(mv)
-      }
-    })
-    input.bytes = cw.toByteArray()
-    return chain.proceed(input)
-  }
-}
+    val classNode = ClassNode()
+    cr.accept(classNode, ClassReader.EXPAND_FRAMES)
 
-class PrimitiveMethodVisitor(mv: MethodVisitor) : BaseMethodVisitor(mv) {
+    for (methodNode in classNode.methods) {
+      for (insnNode in methodNode.instructions.toArray()) {
+        if (insnNode.type == METHOD_INSN && insnNode.opcode == Opcodes.INVOKESTATIC) {
+          val methodInsn = insnNode as MethodInsnNode
+          if (
+            (methodInsn.owner == BYTE_CLASS.toInternalName() && methodInsn.name == "parseByte" && methodInsn.desc == "(Ljava/lang/String;)B") ||
+            (methodInsn.owner == SHORT_CLASS.toInternalName() && methodInsn.name == "parseShort" && methodInsn.desc == "(Ljava/lang/String;)S") ||
+            (methodInsn.owner == INTEGER_CLASS.toInternalName() && methodInsn.name == "parseInt" && methodInsn.desc == "(Ljava/lang/String;)I") ||
+            (methodInsn.owner == LONG_CLASS.toInternalName() && methodInsn.name == "parseLong" && methodInsn.desc == "(Ljava/lang/String;)J") ||
+            (methodInsn.owner == FLOAT_CLASS.toInternalName() && methodInsn.name == "parseFloat") ||
+            (methodInsn.owner == DOUBLE_CLASS.toInternalName() && methodInsn.name == "parseDouble") ||
+            (methodInsn.owner == BOOLEAN_CLASS.toInternalName() && methodInsn.name == "parseBoolean")
+          ) {
+            val index = methodNode.instructions.indexOf(insnNode)
+            val hasCatched = methodNode.tryCatchBlocks.any {
+              val start = methodNode.instructions.indexOf(it.start)
+              val end = methodNode.instructions.indexOf(it.end)
+              val inTryBlock = index in start until end
+              val isCatching = it.type == Type.getInternalName(NumberFormatException::class.java) ||
+                  it.type == Type.getInternalName(IllegalArgumentException::class.java) ||
+                  it.type == Type.getInternalName(RuntimeException::class.java) ||
+                  it.type == Type.getInternalName(Exception::class.java) ||
+                  it.type == Type.getInternalName(Throwable::class.java)
+              inTryBlock && isCatching
+            }
 
-  override fun visitMethodInsn(
-    opcode: Int,
-    owner: String?,
-    name: String?,
-    descriptor: String?,
-    isInterface: Boolean
-  ) {
-
-    if (opcode == Opcodes.INVOKESTATIC) {
-      if (
-        (owner == BYTE_CLASS.toInternalName() && name == "parseByte" && descriptor == "(Ljava/lang/String;)B") ||
-        (owner == SHORT_CLASS.toInternalName() && name == "parseShort" && descriptor == "(Ljava/lang/String;)S") ||
-        (owner == INTEGER_CLASS.toInternalName() && name == "parseInt" && descriptor == "(Ljava/lang/String;)I") ||
-        (owner == LONG_CLASS.toInternalName() && name == "parseLong" && descriptor == "(Ljava/lang/String;)J") ||
-        (owner == FLOAT_CLASS.toInternalName() && name == "parseFloat") ||
-        (owner == DOUBLE_CLASS.toInternalName() && name == "parseDouble") ||
-        (owner == BOOLEAN_CLASS.toInternalName() && name == "parseBoolean")
-      ) {
-        super.visitMethodInsn(
-          Opcodes.INVOKESTATIC,
-          PRIMITIVE_TYPE_PARSER.toInternalName(),
-          name,
-          descriptor,
-          isInterface
-        )
-        return
+            val newInsnNode = MethodInsnNode(
+              Opcodes.INVOKESTATIC,
+              PRIMITIVE_TYPE_PARSER.toInternalName(),
+              if (hasCatched) "${insnNode.name}Throw" else insnNode.name,
+              insnNode.desc,
+              false
+            )
+            methodNode.instructions.set(insnNode, newInsnNode)
+          }
+        }
       }
     }
 
-    super.visitMethodInsn(opcode, owner, name, descriptor, isInterface)
+    val cw = ClassWriter(ClassWriter.COMPUTE_MAXS)
+    classNode.accept(cw)
+    input.bytes = cw.toByteArray()
+    return chain.proceed(input)
   }
 }
--- defensor/src/main/java/com/panda912/defensor/internal/PrimitiveTypeParser.java
+++ defensor/src/main/java/com/panda912/defensor/internal/PrimitiveTypeParser.java
@@ -10,109 +10,158 @@ import com.panda912.defensor.ErrorCode;
  */
 public class PrimitiveTypeParser {
 
-  public static short parseShort(String str) {
-    return parseShort(str, (short) 0);
-  }
-
-  public static int parseInt(String str) {
-    return parseInt(str, 0);
-  }
-
-  public static long parseLong(String str) {
-    return parseLong(str, 0L);
-  }
-
-  public static float parseFloat(String str) {
-    return parseFloat(str, 0.0f);
-  }
-
-  public static double parseDouble(String str) {
-    return parseDouble(str, 0.0d);
-  }
-
-  public static byte parseByte(String str) {
-    return parseByte(str, (byte) 0);
-  }
-
   public static boolean parseBoolean(String str) {
     return parseBoolean(str, false);
   }
 
-  public static short parseShort(String str, short defValue) {
-    if (!TextUtils.isEmpty(str)) {
-      try {
-        return Short.parseShort(str);
-      } catch (NumberFormatException e) {
-        CrashDefensor.onCrash(ErrorCode.IllegalArgumentException, "Short.parseShort() throw IllegalArgumentException", e);
-      }
-    }
-    return defValue;
+  public static boolean parseBooleanThrow(String str) {
+    return parseBoolean(str, true);
   }
 
-  public static int parseInt(String str, int defValue) {
-    if (!TextUtils.isEmpty(str)) {
-      try {
-        return Integer.parseInt(str);
-      } catch (NumberFormatException e) {
-        CrashDefensor.onCrash(ErrorCode.IllegalArgumentException, "Integer.parseInt() throw IllegalArgumentException", e);
-      }
-    }
-    return defValue;
+  public static byte parseByte(String str) {
+    return parseByte(str, false);
   }
 
-  public static long parseLong(String str, long defValue) {
-    if (!TextUtils.isEmpty(str)) {
-      try {
-        return Long.parseLong(str);
-      } catch (NumberFormatException e) {
-        CrashDefensor.onCrash(ErrorCode.IllegalArgumentException, "Long.parseLong() throw IllegalArgumentException", e);
-      }
-    }
-    return defValue;
+  public static byte parseByteThrow(String str) {
+    return parseByte(str, true);
   }
 
-  public static float parseFloat(String str, float defValue) {
-    if (!TextUtils.isEmpty(str)) {
-      try {
-        return Float.parseFloat(str);
-      } catch (NumberFormatException e) {
-        CrashDefensor.onCrash(ErrorCode.IllegalArgumentException, "Float.parseFloat() throw IllegalArgumentException", e);
-      }
-    }
-    return defValue;
+  public static short parseShort(String str) {
+    return parseShort(str, false);
   }
 
-  public static double parseDouble(String str, double defValue) {
-    if (!TextUtils.isEmpty(str)) {
-      try {
-        return Double.parseDouble(str);
-      } catch (NumberFormatException e) {
-        CrashDefensor.onCrash(ErrorCode.IllegalArgumentException, "Double.parseDouble() throw IllegalArgumentException", e);
-      }
-    }
-    return defValue;
+  public static short parseShortThrow(String str) {
+    return parseShort(str, true);
   }
 
-  public static byte parseByte(String str, byte defValue) {
-    if (!TextUtils.isEmpty(str)) {
-      try {
-        return Byte.parseByte(str);
-      } catch (NumberFormatException e) {
-        CrashDefensor.onCrash(ErrorCode.IllegalArgumentException, "Byte.parseByte() throw IllegalArgumentException", e);
-      }
-    }
-    return defValue;
+  public static int parseInt(String str) {
+    return parseInt(str, false);
   }
 
-  public static boolean parseBoolean(String str, boolean defValue) {
+  public static int parseIntThrow(String str) {
+    return parseInt(str, true);
+  }
+
+  public static float parseFloat(String str) {
+    return parseFloat(str, false);
+  }
+
+  public static float parseFloatThrow(String str) {
+    return parseFloat(str, true);
+  }
+
+  public static long parseLong(String str) {
+    return parseLong(str, false);
+  }
+
+  public static long parseLongThrow(String str) {
+    return parseLong(str, true);
+  }
+
+  public static double parseDouble(String str) {
+    return parseDouble(str, false);
+  }
+
+  public static double parseDoubleThrow(String str) {
+    return parseDouble(str, true);
+  }
+
+  private static boolean parseBoolean(String str, boolean isThrow) {
     if (!TextUtils.isEmpty(str)) {
       try {
         return Boolean.parseBoolean(str);
       } catch (NumberFormatException e) {
-        CrashDefensor.onCrash(ErrorCode.IllegalArgumentException, "Boolean.parseBoolean() throw IllegalArgumentException", e);
+        CrashDefensor.onCrash(ErrorCode.IllegalArgumentException, "Boolean.parseBoolean(\"" + str + "\") throw IllegalArgumentException", e);
+        if (isThrow) {
+          throw e;
+        }
       }
     }
-    return defValue;
+    return false;
+  }
+
+  private static byte parseByte(String str, boolean isThrow) {
+    if (!TextUtils.isEmpty(str)) {
+      try {
+        return Byte.parseByte(str);
+      } catch (NumberFormatException e) {
+        CrashDefensor.onCrash(ErrorCode.IllegalArgumentException, "Byte.parseByte(\"" + str + "\") throw IllegalArgumentException", e);
+        if (isThrow) {
+          throw e;
+        }
+      }
+    }
+    return (byte) 0;
+  }
+
+  private static short parseShort(String str, boolean isThrow) {
+    if (!TextUtils.isEmpty(str)) {
+      try {
+        return Short.parseShort(str);
+      } catch (NumberFormatException e) {
+        CrashDefensor.onCrash(ErrorCode.IllegalArgumentException, "Short.parseShort(\"" + str + "\") throw IllegalArgumentException", e);
+        if (isThrow) {
+          throw e;
+        }
+      }
+    }
+    return (short) 0;
+  }
+
+  private static int parseInt(String str, boolean isThrow) {
+    if (!TextUtils.isEmpty(str)) {
+      try {
+        return Integer.parseInt(str);
+      } catch (NumberFormatException e) {
+        CrashDefensor.onCrash(ErrorCode.IllegalArgumentException, "Integer.parseInt(\"" + str + "\") throw IllegalArgumentException", e);
+        if (isThrow) {
+          throw e;
+        }
+      }
+    }
+    return 0;
+  }
+
+  private static float parseFloat(String str, boolean isThrow) {
+    if (!TextUtils.isEmpty(str)) {
+      try {
+        return Float.parseFloat(str);
+      } catch (NumberFormatException e) {
+        CrashDefensor.onCrash(ErrorCode.IllegalArgumentException, "Float.parseFloat(\"" + str + "\") throw IllegalArgumentException", e);
+        if (isThrow) {
+          throw e;
+        }
+      }
+    }
+    return (float) 0.0;
+  }
+
+  private static long parseLong(String str, boolean isThrow) {
+    if (!TextUtils.isEmpty(str)) {
+      try {
+        return Long.parseLong(str);
+      } catch (NumberFormatException e) {
+        CrashDefensor.onCrash(ErrorCode.IllegalArgumentException, "Long.parseLong(\"" + str + "\") throw IllegalArgumentException", e);
+        if (isThrow) {
+          throw e;
+        }
+      }
+    }
+    return 0;
+  }
+
+  private static double parseDouble(String str, boolean isThrow) {
+    if (!TextUtils.isEmpty(str)) {
+      try {
+        return Double.parseDouble(str);
+      } catch (NumberFormatException e) {
+        CrashDefensor.onCrash(ErrorCode.IllegalArgumentException, "Double.parseDouble(\"" + str + "\") throw IllegalArgumentException", e);
+        if (isThrow) {
+          throw e;
+        }
+      }
+    }
+    return 0.0;
   }
 
 }
