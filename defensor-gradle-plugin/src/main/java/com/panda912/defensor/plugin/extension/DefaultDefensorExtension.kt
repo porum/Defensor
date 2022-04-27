f297d289cd146520cb8c790adec9572f6f7c707d
porum <sunguobao12@gmail.com>
2022-04-27 21:23:16 +0800

    modify extension

--- defensor-gradle-plugin/src/main/java/com/panda912/defensor/plugin/extension/DefaultDefensorExtension.kt
+++ defensor-gradle-plugin/src/main/java/com/panda912/defensor/plugin/extension/DefaultDefensorExtension.kt
@@ -4,15 +4,6 @@ package com.panda912.defensor.plugin.extension
  * Created by panda on 2021/9/23 10:52
  */
 open class DefaultDefensorExtension : DefensorExtension {
-
-  var enable: Boolean = true
-  var excludes = listOf<String>()
-
-  override fun enable(enable: Boolean) {
-    this.enable = enable
-  }
-
-  override fun excludes(list: List<String>) {
-    this.excludes = list
-  }
+  override var enable: Boolean = true
+  override var excludes: List<String> = listOf()
 }
--- defensor-gradle-plugin/src/main/java/com/panda912/defensor/plugin/extension/DefensorExtension.kt
+++ defensor-gradle-plugin/src/main/java/com/panda912/defensor/plugin/extension/DefensorExtension.kt
@@ -4,6 +4,6 @@ package com.panda912.defensor.plugin.extension
  * Created by panda on 2021/9/23 10:50
  */
 interface DefensorExtension {
-  fun enable(enable: Boolean)
-  fun excludes(list: List<String>)
+  var enable: Boolean
+  var excludes: List<String>
 }
