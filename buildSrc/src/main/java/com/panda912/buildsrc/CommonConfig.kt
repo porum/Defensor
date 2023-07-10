ccf9db9d900374d8f9842bc5d483c1cf3484c8e9
porum <sunguobao12@gmail.com>
2023-07-10 15:18:51 +0800

    release v1.3.6

--- buildSrc/src/main/java/com/panda912/buildsrc/CommonConfig.kt
+++ buildSrc/src/main/java/com/panda912/buildsrc/CommonConfig.kt
@@ -6,7 +6,7 @@ package com.panda912.buildsrc
 object CommonConfig {
   const val AGP_VERSION = "7.1.3"
   const val KOTLIN_VERSION = "1.6.21"
-  const val DEFENSOR_VERSION = "1.3.5"
+  const val DEFENSOR_VERSION = "1.3.6"
   const val GROUP = "io.github.porum"
   const val compileSdk = 32
   const val minSdk = 21
